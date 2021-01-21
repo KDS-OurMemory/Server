f8b18d3bf27691c36649256140594c575011f0f5
idean3885 <idean3885@naver.com>
2021-01-21 20:23:00 +0900

    feat: 기억공간 서버 프로젝트 생성
    spring boot / gradle / Mysql8

---
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/OurMemoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.kds.ourmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurMemoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurMemoryServerApplication.class, args);
+	}
+
+}
---
+++ OurMemory_Server/src/test/java/com/kds/ourmemory/OurMemoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kds.ourmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurMemoryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
