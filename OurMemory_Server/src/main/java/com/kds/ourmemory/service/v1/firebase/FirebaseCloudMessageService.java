b1d8d1b2f4043f4e6e82a0d2ca9406d5f65c98db
idean3885 <idean3885@naver.com>
2021-02-07 01:25:45 +0900

    feat: slf4j - logback 설정 추가

--- OurMemory_Server/src/main/java/com/kds/ourmemory/service/v1/firebase/FirebaseCloudMessageService.java
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/service/v1/firebase/FirebaseCloudMessageService.java
@@ -2,6 +2,8 @@ package com.kds.ourmemory.service.v1.firebase;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,14 @@ import com.google.common.net.HttpHeaders;
 import com.kds.ourmemory.dto.firebase.FcmMessage;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FirebaseCloudMessageService {
@@ -26,6 +30,7 @@ public class FirebaseCloudMessageService {
 	private final ObjectMapper objectMapper;
 	
 	public void sendMessageTo(String targetToken, String title, String body) {
+		Response response = null;
 		try {
 			String message = makeMessage(targetToken, title, body);
 			
@@ -38,14 +43,20 @@ public class FirebaseCloudMessageService {
 					.addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
 					.build();
 			
-			Response response = client.newCall(request).execute();
+			response = client.newCall(request).execute();
 			
-			System.out.println(response.body().string());
+			log.debug(response.body().string());
 		} catch (JsonProcessingException e) {
-			System.out.println(e.toString());
-			System.out.println("makeMessage Failed.");
+			log.error(e.toString());
+			log.error("makeMessage Failed.");
 		} catch (IOException e) {
-			System.out.println(e.toString());
+			log.error(e.toString());
+		} finally {
+			Consumer<Response> bodyClose = rsp -> Optional.ofNullable(rsp).ifPresent(r -> r.body().close());
+			Consumer<Response> responseClose = rsp -> Optional.ofNullable(rsp).ifPresent(r -> r.close());
+			
+			bodyClose.accept(response);
+			responseClose.accept(response);
 		}
 	}
 	
