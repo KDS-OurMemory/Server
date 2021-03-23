d1b99d09b7ca0bb08cf86d6050d1677a56f6aac1
dykim <idean3885@naver.com>
2021-03-23 17:45:11 +0900

    fix: 예외처리 추가, 예외 코드값 및 메시지 추가

--- OurMemory_Server/src/main/java/com/kds/ourmemory/advice/CRestControllerAdvice.java
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/advice/CRestControllerAdvice.java
@@ -4,9 +4,11 @@ import static com.kds.ourmemory.controller.v1.ApiResult.error;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -36,35 +38,49 @@ public class CRestControllerAdvice {
 	@ExceptionHandler(CUserNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ResponseEntity<?> handleCNotFoundUserException(CUserNotFoundException e) {
-	    log.warn("Not Found User From Users. plz check userId.\n {}", e.getMessage());
-		return response(getMessage("userNotFound.code"), getMessage("userNotFound.msg"), HttpStatus.NOT_FOUND);
+	    log.warn(e.getMessage());
+		return response(getMessage("user.notFound.code"), e.getMessage(), HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(CUserException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> handleCUsersException(CUserException e) {
         log.warn(e.getMessage(), e);
-        return response(getMessage("unKnown.code"), getMessage("unKnown.msg"), HttpStatus.INTERNAL_SERVER_ERROR);
+        return response(getMessage("unKnown.code"), e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 	
 	@ExceptionHandler(CRoomException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ResponseEntity<?> handleCRoomsException(CRoomException e) {
 	    log.warn(e.getMessage(), e);
-	    return response(getMessage("userNotFound.code"), getMessage("userNotFound.msg"), HttpStatus.NOT_FOUND);
+	    return response(getMessage("unKnown.code"), e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(CMemoryException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<?> handleCMemorysException(CMemoryException e) {
         log.warn(e.getMessage(), e);
-        return response(getMessage("unKnown.code"), getMessage("unKnown.msg"), HttpStatus.INTERNAL_SERVER_ERROR);
+        return response(getMessage("unKnown.code"), e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+	
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<?> handleBadParameterException(MissingServletRequestParameterException e) {
+	    log.warn(e.getMessage());
+	    return response(getMessage("queryString.badParameter.code"), e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(IncorrectResultSizeDataAccessException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<?> handleDataSizeException(IncorrectResultSizeDataAccessException e) {
+	    log.warn(e.getMessage());
+	    return response(getMessage("DB.incorrectResultSize.code"), e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
 	
 	@ExceptionHandler({Exception.class, RuntimeException.class})
 	public ResponseEntity<?> handleException(Exception e) {
 	    log.error("Unexpected exception occurred: {}", e.getMessage(), e);
-	    return response(getMessage("unKnown.code"), getMessage("unKnown.msg"), HttpStatus.INTERNAL_SERVER_ERROR); 
+	    return response(getMessage("unKnown.code"), e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR); 
 	}
 	
 	private String getMessage(String code) {
--- OurMemory_Server/src/main/java/com/kds/ourmemory/util/DateUtil.java
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/util/DateUtil.java
@@ -7,8 +7,6 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import org.apache.commons.lang3.time.DateUtils;
-
 public class DateUtil {
     private DateUtil() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
@@ -16,7 +14,7 @@ public class DateUtil {
     }
     
     public static String currentDate() {
-        return new SimpleDateFormat("yyyyMMdd").format(DateUtils.addHours(new Date(), 9));
+        return new SimpleDateFormat("yyyyMMdd").format(new Date(System.currentTimeMillis() + 9 * 1000 * 60 * 60));
     }
     
     public static Date currentTime(){
@@ -24,7 +22,7 @@ public class DateUtil {
         try {
             DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
             
-            return format.parse(format.format(new Date()));
+            return format.parse(format.format(new Date(System.currentTimeMillis() + 9 * 1000 * 60 * 60)));
         } catch (ParseException e) {
             e.printStackTrace();
         }
