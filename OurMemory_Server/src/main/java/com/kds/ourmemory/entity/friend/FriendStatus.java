d52078c50b73e8de62c0591f591d61779c039b61
idean <idean3885@naver.com>
2021-06-11 23:54:09 +0900

    fix: 친구 상태에 맞춰 요청, 추가, 삭제 되도록 수정, 테스트 코드 작성 중

--- OurMemory_Server/src/main/java/com/kds/ourmemory/entity/friend/FriendStatus.java
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/entity/friend/FriendStatus.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum FriendStatus {
     WAIT("친구 요청 후 대기상태"),
+    REQUESTED_BY("친구 요청을 받은 상태"),
     FRIEND("친구 추가 완료된 상태, 상대방에서 친구 삭제한 여부는 파악할 수 없음"),
-    BLOCK("내가 차단한 사람")
+    BLOCK("차단한 상태")
     ;
 
     private final String desc;
