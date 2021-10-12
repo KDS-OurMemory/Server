99faa42f349439ff41618b60ef0ae7edc60aa518
dykim <dykimDev3885@gmail.com>
2021-10-12 22:47:33 +0900

    fix: 친구 수락 기능 스웨거 설명 수정 -> 친구 요청 관련 알림 삭제 설명 추가

--- OurMemory_Server/src/main/java/com/kds/ourmemory/controller/v1/friend/FriendController.java
+++ OurMemory_Server/src/main/java/com/kds/ourmemory/controller/v1/friend/FriendController.java
@@ -32,7 +32,10 @@ public class FriendController {
         return ok(friendService.cancelFriend(request));
     }
 
-    @ApiOperation(value = "친구 수락", notes = "친구 요청을 수락하고 친구를 추가한다. 요청한 사람/요청받은 사람 모두 친구 추가 진행.")
+    @ApiOperation(value = "친구 수락",
+            notes = "친구 요청을 수락하고 친구를 추가한다. " +
+                    "요청한 사람/요청받은 사람 모두 친구 추가를 진행하며 요청을 수락한 사용자 알림 중 친구 요청 알림을 삭제한다."
+    )
     @PostMapping(value = "/accept")
     public ApiResult<AcceptFriendDto.Response> acceptFriend(@RequestBody AcceptFriendDto.Request request) {
         return ok(friendService.acceptFriend(request));
