405ae9ed4fa2865be56adda3d29c7bee3e6203c6
dykim <dykimDev3885@gmail.com>
2022-03-05 23:24:58 +0900

    fix: 로깅 테스트 코드 중 프로필이미지 업로드 삭제
    RestTemplate 의 기본 MessageConverters 가 MultipartFile 에 포함된 InputStream 을 직렬화하지 못해 요청이 실패한다.
    이를 해결하기 위해선 MultiValueMap 으로 리퀘스트 바디 자료구조를 변경하는 방법이 있다.
    이 경우, 관련 로직 및 테스트 코드를 전부 수정해야 하기 때문에 우선 멀티파트 파일 테스트 코드는 삭제하도록 한다.

--- OurMemory_Server/src/test/java/com/kds/ourmemory/v1/config/HttpLogInterceptorTest.java
+++ OurMemory_Server/src/test/java/com/kds/ourmemory/v1/config/HttpLogInterceptorTest.java
@@ -11,15 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.LinkedMultiValueMap;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,9 +19,17 @@ import static org.assertj.core.api.Assertions.assertThat;
  * 인터셉터를 통해 로깅이 정상적으로 이루어지는지 테스트하기 위한 클래스
  *
  * 각 요청의 상세 로직은 서비스테스트코드를 통해 검증되기 때문에
- * 성공 / 실패 / 특이 케이스(멀티파트 파일 요청) 의 사례 하나씩만 작성함.
- * 참고) 멀티파트 파일의 경우, 로깅을 위해 스트림을 미리 읽어 멀티파트핸들러 작업 시 오류가 발생한다.
- *      따라서 멀티파트 파일을 포함한 요청은 데이터를 로깅하지 않으며, 이러한 특이사항을 테스트하기 위해 케이스를 추가했다.
+ * 성공 / 실패 / 특이 케이스 의 사례 하나씩만 작성함.
+ *
+ * 참고) 특이케이스
+ * 1. 멀티파트 파일 요청
+ *   예 - 프로필이미지 업로드
+ *    멀티파트 파일의 경우, 로깅을 위해 스트림을 미리 읽어 멀티파트핸들러 작업 시 오류가 발생한다.
+ *    따라서 멀티파트 파일을 포함한 요청은 데이터를 로깅하지 않으며, 이러한 특이사항을 테스트하기 위해 케이스를 추가했다.
+ *    => 22.03.05 MultipartFile 의 경우
+ *    TestRestTemplate 의 기본 MessageConverters 가 MultipartFile 에 포함된 InputStream 을 직렬화하지 못해 예외가 발생한다.
+ *    이를 해결하기 위해선 MultipartFile 대신 MultiValueMap 에 file 데이터를 넣어 전달하는 방식으로 수정이 필요하다.
+ *    이 경우, 파일업로드 로직 및 관련 서비스 코드와 테스트 코드 모두 수정해야 하기 때문에 범위가 넓어 보류함.
  */
 @ActiveProfiles("test")
 @Slf4j
@@ -68,30 +68,6 @@ class HttpLogInterceptorTest {
         var notFoundUserId = -1;
         var url = "/v1/users/" + notFoundUserId;
         var apiResult = restTemplate.getForObject(url, ApiResult.class);
-        assertThat(apiResult.getResultCode()).isEqualTo("U002"); // 사용자를 찾을 수 없습니다.
-        log.debug(prettyPrint(apiResult));
-    }
-
-    @Test
-    @DisplayName("프로필이미지 업로드 | 실패 | 사용자를 찾을 수 없음.")
-    void profileImageUploadFailToNotFoundUser() throws IOException {
-        // 성공 케이스를 테스트할 경우, S3 에 실제 데이터가 업로드되기 때문에 실패 케이스를 테스트하여 데이터 로깅 여부만 확인한다.
-        var file = new MockMultipartFile("request",
-                "fake File",
-                "image/jpg",
-                new FileInputStream("src/test/java/com/kds/ourmemory/v1/config/HttpLogInterceptorTest.java")
-        );
-
-        var httpHeaders = new HttpHeaders();
-        var requestBody = new LinkedMultiValueMap<String, Object>();
-        requestBody.set("profileImage", file);
-        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-        var requestEntity = new HttpEntity<>(requestBody, httpHeaders);
-        var notFoundUserId = -1;
-        var url = String.format("/v1/users/%d/profileImage", notFoundUserId);
-
-        var apiResult = restTemplate.postForObject(url, requestEntity, ApiResult.class);
         assertThat(apiResult.getResultCode()).isEqualTo("U002"); // 사용자를 찾을 수 없습니다.
         log.debug(prettyPrint(apiResult));
     }
