e1f144a2a653fd8b6db3611a65fb3fdb8180c268
dykim <dykimDev3885@gmail.com>
2022-03-14 21:22:32 +0900

    fix: test 프로파일에 테스트용 설정추가, S3 설정 환경변수로 변경, FCM key 추가
    GitHubAction 의 경우, 독립적인 가상환경에서 푸시된 소스코드만 빌드하기 때문에 테스트에 필요한 개인설정 파일을 추가할 수 없다.
    이를 해결하기 위해 GitHubAction 환경변수 설정 옵션을 활용하여 개인정보 설정을 test 프로파일에 넣어 테스트할 수 있도록 수정함.
    FCM key 의 경우, 푸시 후 배포에서 제외시킨다.(추후 AWS 람다로 관리할 예정)

--- OurMemory_Server/src/test/java/com/kds/ourmemory/v1/util/S3UploaderTest.java
+++ OurMemory_Server/src/test/java/com/kds/ourmemory/v1/util/S3UploaderTest.java
@@ -1,12 +1,12 @@
 package com.kds.ourmemory.v1.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,7 +14,7 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Slf4j
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class S3UploaderTest {
